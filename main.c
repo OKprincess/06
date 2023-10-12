78a27b19a5153471cabd01c561bc2d1275672f6d
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-10-12 15:49:34 +0900

    06_P2

--- main.c
+++ main.c
@@ -1,40 +1,27 @@
 //
 //  main.c
-//  06_P1
+//  06_P2 함수의 동작 관찰
 //
 //  Created by ok sojoung on 2023/10/12.
 //
 
-// #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-/*
-int main(void)
-{
-    int i;
-    
-    for( i=0; i<10; i++ )
-        printf("*");
-    for( i=0; i<10; i++ )
-        printf("*");
-    for( i=0; i<10; i++ )
-        printf("*");
-    return 0;
-}
-*/
 
-void print_star(void)   // 공통 부분을 함수로 정의
-{
-    int i;
-    for( i=0; i<10; i++ )
-        printf("*");
+void func1(void){
+    int x;
+    printf("func1 x is at %p\n", &x);   // &x는 메모리의 주소(하드웨어, 노트북 시스템에 따라 다름)
 }
 
-int main(void)          // 간단하게 함수 호출
-{
-    print_star();
-    print_star();
-    print_star();
+int main(void){
+    int x;
+    printf("main  x is at %p\n", &x);
+    func1();
     
+    /*  메모리 주소(&x)가 서로 다르게 나옴
+        이름이 같아도 서로 다른 함수의 변수는 다른 변수다. 즉, x는 서로 다른거임.
+        main함수의 메모리 영역이 더 높은 값임. 제일 먼저 부르는 함수가 제일 밑에(번지수 큼).
+        func함수의 메모리 번지수가 더 작음.
+    */
     return 0;
 }

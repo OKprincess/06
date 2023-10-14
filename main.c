8aa483bb93252a4e7cc142b4d71afd6ab5ad4bd6
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-10-14 13:25:37 +0900

    06_P5

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  06_P4 Call by value
+//  06_P5 Combination
 //
 //  Created by ok sojoung on 2023/10/12.
 //
@@ -8,28 +8,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-void square(int a)
+int factorial(int n)
 {
-    a = a*a;                    // square(2)=4가 나옴.
+    int res = 1;
+    int i;
+    for (i=1; i<n+1; i++)
+    {
+        res = res*i;
+    }
+    return res;
 }
+
+
+int combination(int n, int r)
+{
+    int div1, div2;
+    
+    div1 = factorial(n);
+    div2 = factorial(n-r)*factorial(r);
+    
+    return (div1/div2);
+}
+
 
 int main(void)
 {
-    int a = 2;                  //스텍에 제일 아래에 main(a=2)이 먼저 오고 그 위에 스텍영역에 계산된 4값이있음.
-    square(a);
-    printf("a = %i\n",a);       // 결과가 2로 나옴.
-}
-*/
-
-int square(int a)
-{
-    return (a*a);               // 함수의 결과를 쓰고 싶을 땐 무조건 return 사용!!
-}
-
-int main(void)
-{
-    int a=2;                    // 스텍 제일 아래부분에 2가 들어가있음.
-    a = square(a);              // 그 위에 2가 들어가서 a=4가 됨.
-    printf("a = %i\n", a);
+    // 1. 입력값 받음
+    int n, r;
+    printf("input n :");
+    scanf("%d", &n);
+    printf("input r :");
+    scanf("%d", &r);
+    
+    // 2. 분모 분자 계산
+    int result;
+    result = combination(n, r);
+    
+    // 3. 최종값 출력
+    printf("Result of combination: %d\n", result);
+    return 0;
 }
